38e436c41bb5c3bc4a79edd4e449dd92cde6bf87
yu-tung618 <a0189174@gmail.com>
2025-11-20 12:53:39 +0800

    移除

--- home.js
+++
@@ -1,8 +0,0 @@
-let header = document.getElementById("header");
-window.addEventListener("scroll", function(){
-    if(this.window.scrollY>50){
-        header.classList.add("smallPanel ");
-    }else{
-        header.classList.remove("smallPanel ");
-    }
-})
