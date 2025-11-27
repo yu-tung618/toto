3bfe3a7a5d23972ef8b5d615ce25b8bd36eedf03
yu-tung618 <a0189174@gmail.com>
2025-11-27 20:46:09 +0800

    新增飼主回饋的動畫

---
+++ feedback.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+    if(window.innerHeight>=350){
+        const animatedElements =document.querySelectorAll(".left,.right")
+   
+
+    function triggerAnimationOnScroll(){
+        const windowHeight =window.innerHeight;
+
+        animatedElements.forEach(element=>{
+            const elementTopPosition =element.getBoundingClientRect().top;
+
+            if(elementTopPosition<windowHeight*0.8){
+                element.classList.add("show");
+            }
+        })
+ };
+        triggerAnimationOnScroll();
+        window.addEventListener("scroll",triggerAnimationOnScroll);
+    }
+
+
+
+}
