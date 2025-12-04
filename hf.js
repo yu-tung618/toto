74a7993912e3a3121c7c90edcf485f1523f9bd53
yu-tung618 <a0189174@gmail.com>
2025-12-04 13:48:04 +0800

    更新選單固定頁面顏色

--- hf.js
+++ hf.js
@@ -23,3 +23,13 @@ overlay.addEventListener('click', () => {
   checkbox.checked = false;
   overlay.classList.remove('active');
 });
+
+//選單顏色
+  const page = window.location.pathname.split("/"
+  ).pop();//window.location.pathname → 取得目前網址的路徑部分,split("/")把字串用 / 切開，變成陣列：["", "about.html"],pathname 就是 /about.html(之類的)
+  const links = document.querySelectorAll("nav a");
+  links.forEach(link =>{
+    if(link.getAttribute("href") === page){
+      link.classList.add("change");//getAttribute("href")取得這個 <a> 的 href 屬性值
+    }
+  })
