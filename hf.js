85763b8aeb0ee9e8f54d14ab20eb3d5a150253b9
yu-tung618 <a0189174@gmail.com>
2025-12-06 19:55:07 +0800

    修改漢堡選單

--- hf.js
+++ hf.js
@@ -1,28 +1,11 @@
 
 const btn = document.querySelector('.menu-btn');
-const btnClose = document.querySelector('.menu-btn1');
 const overlay = document.getElementById('overlay');
-const checkbox = document.getElementById('menu-control');
 
-// 打開選單
-btn.addEventListener('click', (e) => {
-  e.preventDefault(); // 阻止跳到頂部
-  checkbox.checked = true;
-  overlay.classList.add('active');
-});
 
-// 關閉選單
-btnClose.addEventListener('click', (e) => {
-  e.preventDefault();
-  checkbox.checked = false;
-  overlay.classList.remove('active');
-});
-
-// 點灰色背景也能關閉
-overlay.addEventListener('click', () => {
-  checkbox.checked = false;
-  overlay.classList.remove('active');
-});
+btn.addEventListener("click",function(){
+  overlay.classList.toggle("active");
+})
 
 //選單顏色
   const page = window.location.pathname.split("/"
